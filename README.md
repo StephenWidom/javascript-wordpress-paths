9e6972a8c46d09328bb8af80297a0ae8d08ce6dd
Stephen Widom <stevewidom@gmail.com>
2015-08-04 12:39:41 -0400

    Change js var prefix to jwp_
    To avoid variables starting with 'wp_wp_', the javascript variable prefix
    has been changed to 'jwp_'. Also updated README.md to increase
    readability.

--- README.md
+++ README.md
@@ -5,37 +5,37 @@ A WordPress plugin to define WordPress paths, like site_url(), as JavaScript var
 JavaScript variables on the left, their PHP equivalent on the right.
 
 ### Plugins
-```wp_plugins_url``` = ```plugins_url()```
+ ```wp_plugins_url``` = ```plugins_url()```
 
 ### Themes
-```wp_get_template_directory_uri``` = ```get_template_directory_uri()```
-```wp_get_stylesheet_directory_uri``` = ```get_stylesheet_directory_uri()```
-```wp_get_stylesheet_uri``` = ```get_stylesheet_uri()```
-```wp_get_theme_root_uri``` = ```get_theme_root_uri()```
-```wp_get_theme_root``` = ```get_theme_root()```
-```wp_get_theme_roots``` = ```get_theme_roots()```
-```wp_get_stylesheet_directory``` = ```get_stylesheet_directory()```
-```wp_get_template_directory``` = ```get_template_directory()```
+ ```wp_get_template_directory_uri``` = ```get_template_directory_uri()```
+ ```wp_get_stylesheet_directory_uri``` = ```get_stylesheet_directory_uri()```
+ ```wp_get_stylesheet_uri``` = ```get_stylesheet_uri()```
+ ```wp_get_theme_root_uri``` = ```get_theme_root_uri()```
+ ```wp_get_theme_root``` = ```get_theme_root()```
+ ```wp_get_theme_roots``` = ```get_theme_roots()```
+ ```wp_get_stylesheet_directory``` = ```get_stylesheet_directory()```
+ ```wp_get_template_directory``` = ```get_template_directory()```
 
 ### Site Home
-```wp_home_url``` = ```home_url()```
+ ```wp_home_url``` = ```home_url()```
 
 ### WordPress
-```wp_admin_url``` = ```admin_url()```
-```wp_site_url``` = ```site_url()```
-```wp_content_url``` = ```content_url()```
-```wp_includes_url``` = ```includes_url()```
-```wp_wp_upload_dir_path``` = ```wp_upload_dir["path"]```
-```wp_wp_upload_dir_url``` = ```wp_upload_dir["url"]```
-```wp_wp_upload_dir_basedir``` = ```wp_upload_dir["basedir"]```
-```wp_wp_upload_dir_baseurl``` = ```wp_upload_dir["baseurl"]```
+ ```wp_admin_url``` = ```admin_url()```
+ ```wp_site_url``` = ```site_url()```
+ ```wp_content_url``` = ```content_url()```
+ ```wp_includes_url``` = ```includes_url()```
+ ```wp_wp_upload_dir_path``` = ```wp_upload_dir["path"]```
+ ```wp_wp_upload_dir_url``` = ```wp_upload_dir["url"]```
+ ```wp_wp_upload_dir_basedir``` = ```wp_upload_dir["basedir"]```
+ ```wp_wp_upload_dir_baseurl``` = ```wp_upload_dir["baseurl"]```
 
 ### Multisite
-```wp_get_admin_url``` = ```get_admin_url()```
-```wp_get_home_url``` = ```get_home_url()```
-```wp_get_site_url``` = ```get_site_url()```
-```wp_network_admin_url``` = ```network_admin_url()```
-```wp_network_site_url``` = ```network_site_url()```
-```wp_network_home_url``` = ```network_home_url()```
+ ```wp_get_admin_url``` = ```get_admin_url()```
+ ```wp_get_home_url``` = ```get_home_url()```
+ ```wp_get_site_url``` = ```get_site_url()```
+ ```wp_network_admin_url``` = ```network_admin_url()```
+ ```wp_network_site_url``` = ```network_site_url()```
+ ```wp_network_home_url``` = ```network_home_url()```
 
 *Developed by Stephen Widom - http://stephenwidom.com*
--- javascript-wordpress-paths.php
+++ javascript-wordpress-paths.php
@@ -13,39 +13,39 @@ defined('ABSPATH') or die('No direct script access!');
 function javascript_wordpress_paths(){ ?>
 	<script>
 	//Plugins
-	var wp_plugins_url = '<?php echo plugins_url(); ?>';
+	var jwp_plugins_url = '<?php echo plugins_url(); ?>';
 
 	// Themes
-	var wp_get_template_directory_uri = '<?php echo get_template_directory_uri(); ?>';
-	var wp_get_stylesheet_directory_uri = '<?php echo get_stylesheet_directory_uri(); ?>';
-	var wp_get_stylesheet_uri = '<?php echo get_stylesheet_uri(); ?>';
-	var wp_get_theme_root_uri = '<?php echo get_theme_root_uri(); ?>';
-	var wp_get_theme_root = '<?php echo get_theme_root(); ?>';
-	var wp_get_theme_roots = '<?php echo get_theme_roots(); ?>';
-	var wp_get_stylesheet_directory = '<?php echo get_stylesheet_directory(); ?>';
-	var wp_get_template_directory = '<?php echo get_template_directory(); ?>';
+	var jwp_get_template_directory_uri = '<?php echo get_template_directory_uri(); ?>';
+	var jwp_get_stylesheet_directory_uri = '<?php echo get_stylesheet_directory_uri(); ?>';
+	var jwp_get_stylesheet_uri = '<?php echo get_stylesheet_uri(); ?>';
+	var jwp_get_theme_root_uri = '<?php echo get_theme_root_uri(); ?>';
+	var jwp_get_theme_root = '<?php echo get_theme_root(); ?>';
+	var jwp_get_theme_roots = '<?php echo get_theme_roots(); ?>';
+	var jwp_get_stylesheet_directory = '<?php echo get_stylesheet_directory(); ?>';
+	var jwp_get_template_directory = '<?php echo get_template_directory(); ?>';
 
 	// Site Home
-	var wp_home_url = '<?php echo home_url(); ?>';
+	var jwp_home_url = '<?php echo home_url(); ?>';
 
 	// WordPress
-	var wp_admin_url = '<?php echo admin_url(); ?>';
-	var wp_site_url = '<?php echo site_url(); ?>';
-	var wp_content_url = '<?php echo content_url(); ?>';
-	var wp_includes_url = '<?php echo includes_url(); ?>';
+	var jwp_admin_url = '<?php echo admin_url(); ?>';
+	var jwp_site_url = '<?php echo site_url(); ?>';
+	var jwp_content_url = '<?php echo content_url(); ?>';
+	var jwp_includes_url = '<?php echo includes_url(); ?>';
 	<?php $upload_dir = wp_upload_dir(); // Upload directory paths ?>
-	var wp_wp_upload_dir_path = '<?php echo $upload_dir["path"]; ?>';
-	var wp_wp_upload_dir_url = '<?php echo $upload_dir["url"]; ?>';
-	var wp_wp_upload_dir_basedir = '<?php echo $upload_dir["basedir"]; ?>';
-	var wp_wp_upload_dir_baseurl = '<?php echo $upload_dir["baseurl"]; ?>';
+	var jwp_wp_upload_dir_path = '<?php echo $upload_dir["path"]; ?>';
+	var jwp_wp_upload_dir_url = '<?php echo $upload_dir["url"]; ?>';
+	var jwp_wp_upload_dir_basedir = '<?php echo $upload_dir["basedir"]; ?>';
+	var jwp_wp_upload_dir_baseurl = '<?php echo $upload_dir["baseurl"]; ?>';
 
 	// Multisite
-	var wp_get_admin_url = '<?php echo get_admin_url(); ?>';
-	var wp_get_home_url = '<?php echo get_home_url(); ?>';
-	var wp_get_site_url = '<?php echo get_site_url(); ?>';
-	var wp_network_admin_url = '<?php echo network_admin_url(); ?>';
-	var wp_network_site_url = '<?php echo network_site_url(); ?>';
-	var wp_network_home_url = '<?php echo network_home_url(); ?>';
+	var jwp_get_admin_url = '<?php echo get_admin_url(); ?>';
+	var jwp_get_home_url = '<?php echo get_home_url(); ?>';
+	var jwp_get_site_url = '<?php echo get_site_url(); ?>';
+	var jwp_network_admin_url = '<?php echo network_admin_url(); ?>';
+	var jwp_network_site_url = '<?php echo network_site_url(); ?>';
+	var jwp_network_home_url = '<?php echo network_home_url(); ?>';
 	</script>
 <?php }
 add_action('wp_head','javascript_wordpress_paths',1);
